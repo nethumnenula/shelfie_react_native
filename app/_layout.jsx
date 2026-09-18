d4e1024fcea63199f65dcc085bfe8d5f6c051a32
Nethum <nethumnenula2004@gmail.com>
2026-09-18 21:05:43 +0530

    Expo theme handler

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Platform,
+  StatusBar as RNStatusBar,
+} from "react-native";
+import { Slot, Stack } from "expo-router";
+import { Colors } from "../constants/Colors.js";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  //console.log(colorScheme)
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
-    
-        <Stack screenOptions={{
-            headerStyle: {backgroundColor: '#ddd'},
-            headerTintColor: '#333'
-        }}>
-            <Stack.Screen name='index' options={{ title: 'Home'}}/>
-            <Stack.Screen name='About' options={{ title: 'About'}}/>
-            <Stack.Screen name='Contact' options={{ title: 'Contact'}}/>
-        </Stack>
+    <>
+      {Platform.OS === "android" && (
+        <RNStatusBar
+          translucent={true}
+          barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+          backgroundColor={theme.navBackground}
+        />
+      )}
       
-  )
-}
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+          headerTitleAlign: "center",
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="About" options={{ title: "About" }} />
+        <Stack.Screen name="Contact" options={{ title: "Contact" }} />
+      </Stack>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
